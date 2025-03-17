ea852bd1b7c3898d43cbcb2bea4b6c9f8f92281d
changchanghwang <hch950627@naver.com>
2025-03-17 12:24:47 +0900

    refactor: modify server struct, add handler
    graceful shutdown in main
    add mux handler
    add server test

--- cmd/exporter/main.go
+++ cmd/exporter/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
+	"context"
+	"net/http"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Ecube-Labs/kafka-connect-exporter/internal/collector"
 	"github.com/Ecube-Labs/kafka-connect-exporter/internal/config"
+	exporter "github.com/Ecube-Labs/kafka-connect-exporter/internal/exporter/prometheus"
 	"github.com/Ecube-Labs/kafka-connect-exporter/internal/server"
+	applicationError "github.com/Ecube-Labs/kafka-connect-exporter/pkg/application-error"
 	"github.com/Ecube-Labs/kafka-connect-exporter/pkg/logger"
 )
 
 func main() {
-	server := server.New(config.Port)
+	collector := collector.New(&http.Client{Timeout: 10 * time.Second})
+	exporter := exporter.New(collector)
+
+	mux := http.NewServeMux()
+	mux.Handle(config.MetricsEndpoint, exporter.Handler())
+	mux.HandleFunc(config.HealthCheckEndpoint, func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	server := server.New(config.Port, mux)
 	logger.Log("info", "Starting Kafka Connect Exporter")
-	server.Run()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	go func() {
+		if err := server.Run(); err != nil {
+			logger.Log("error", applicationError.UnWrap(err).Stack)
+		}
+	}()
+
+	<-ctx.Done()
+	logger.Log("info", "Server is shutting down...")
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(shutdownCtx); err != nil {
+		logger.Log("error", applicationError.UnWrap(err).Stack)
+	}
 }
--- internal/server/server.go
+++ internal/server/server.go
@@ -4,54 +4,35 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/Ecube-Labs/kafka-connect-exporter/internal/collector"
-	"github.com/Ecube-Labs/kafka-connect-exporter/internal/config"
-	exporter "github.com/Ecube-Labs/kafka-connect-exporter/internal/exporter/prometheus"
+	applicationError "github.com/Ecube-Labs/kafka-connect-exporter/pkg/application-error"
 	"github.com/Ecube-Labs/kafka-connect-exporter/pkg/logger"
 )
 
 type Server struct {
-	server *http.Server
+	httpServer *http.Server
 }
 
-func New(port string) *Server {
+func New(port string, handler http.Handler) *Server {
 	return &Server{
-		server: &http.Server{Addr: ":" + port},
+		httpServer: &http.Server{
+			Addr:    ":" + port,
+			Handler: handler,
+		},
 	}
 }
 
-func (s *Server) Run() {
-	collector := collector.New(&http.Client{Timeout: 10 * time.Second})
-	exporter := exporter.New(collector)
-
-	http.Handle(config.MetricsEndpoint, exporter.Handler())
-	http.HandleFunc(config.HealthCheckEndpoint, func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	go func() {
-		if err := s.server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Log("error", fmt.Sprintf("HTTP Server Error: %s", err))
-		}
-	}()
-
-	<-ctx.Done()
-	logger.Log("info", "Server is shutting down...")
-
-	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := s.server.Shutdown(shutdownCtx); err != nil {
-		logger.Log("error", fmt.Sprintf("Server Shutdown Error: %s", err))
-	} else {
-		logger.Log("info", "Server has been safely shutdown")
+func (s *Server) Run() error {
+	if err := s.httpServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		return applicationError.New(http.StatusInternalServerError, fmt.Sprintf("Failed to start server: %s", err.Error()), "")
 	}
+	return nil
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	if err := s.httpServer.Shutdown(ctx); err != nil {
+		return applicationError.New(http.StatusInternalServerError, fmt.Sprintf("Failed to shutdown server: %s", err.Error()), "")
+	}
+	logger.Log("info", "Server has been safely shutdown")
+	return nil
 }
